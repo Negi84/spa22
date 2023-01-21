0121ebfe2b86c9c5e49e15d79db6a16ed02b6f9b
Vipin Negi <vipin@mindrops.com>
2023-01-21 20:48:24 +0530

    layouts & nested routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -14,8 +18,22 @@ import ProductsPage from "./pages/Products";
 // const router =createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+  
+  //we can also easily add another layout
+  // {
+  //   path: '/admin',
+  //   children: [
+
+  //   ]
+  // }
 ]);
 
 function App() {
